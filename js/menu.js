5ff6798da0b424c203951157c745553b9aea2ec3
YAN GUILHERME TEIXEIRA <yan.teixeira@flexcontact.com.br>
2020-02-06 17:41:55 -0300

    menu lateral mobile feito

---
+++ js/menu.js
@@ -0,0 +1,9 @@
+function ativar() {
+    var menu = document.getElementById("menu");
+    menu.classList.add("menu-ativo");
+  }
+
+  function desativar() {
+    var menu = document.getElementById("menu");
+    menu.classList.remove("menu-ativo");
+  }
